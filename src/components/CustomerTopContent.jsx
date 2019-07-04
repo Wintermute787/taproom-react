6332b4bdb95724b2d9929c474edb7ac8a4893409
Joe Barnes <joebarnes333@gmail.com>
2019-07-04 15:12:59 -0700

    add customer section

---
+++ src/components/CustomerTopContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CustomerTopContent() {
+    return(
+        <div>
+            <div className='header'></div>
+            <div className='sideNav'></div>
+            <div className='content'></div>
+        </div>
+    )
+}
+
