16d15cf4212f43ddb414d0e2a86979ca1e933424
Joe Barnes <joebarnes333@gmail.com>
2019-07-04 09:56:17 -0700

    add more styling to hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,28 +2,49 @@ import React from 'react'
 import image1 from './assets/image1.jpg'
 import { makeStyles } from '@material-ui/core/styles';
 
+{/*<img src={require('./assets/image1.jpg')} alt="" style={heroStyle}/>*/}
+
 var heroStyle = {
     height: '100vh',
     width: '100vw',
-    objectFit: 'cover',
-    position: "relative"
+
+    backgroundColor: "rgba(0,0,0,0.33)",
+    zIndex: "10"
+
+
 };
 var heroBox = {
+    minHeight: "100%",
+    minWidth: '1024px',
+    width: "100%",
+    height: "auto",
+    top: "0",
+    left: "0",
     position: "absolute",
-    top: "50%",
-    left: "35%"
+    backgroundImage: `url(${image1})`,
+    zIndex: '-1',
+    backgroundSize: "cover"
 
+};
+
+var heroText = {
+    color: 'white',
+    zIndex: "14",
+    position: "absolute",
+    top: "45%",
+    left: "20%",
+    textTransform: "uppercase",
+    letterSpacing: "15px"
 }
+
 
 
 function Hero() {
 
     return(
-        <div>
-            <img src={require('./assets/image1.jpg')} alt="" style={heroStyle}/>
-            <div style={heroBox}>
-                <h1>Experience our winter cold brew</h1>
-            </div>
+        <div style={heroStyle}>
+            <div style={heroBox}></div>
+            <h1 style={heroText}>Experience our winter cold brew</h1>
         </div>
     )
 }
